16ff91af8139a7bc88c3609ef6f66fd71ceae4e8
jokoho48 <hoffmann.jonas@gmail.com>
2015-08-02 02:29:34 +0200

    Fix Config issue

--- JK_TemplateMission.VR/lib/Core/cfgFunctions.hpp
+++ JK_TemplateMission.VR/lib/Core/cfgFunctions.hpp
@@ -4,13 +4,17 @@ class JK_Core
     {
         file = "lib\Core";
         class gm { postInit = 1; };
-        class crate {};
+        class crate;
         class getWeight;
         class preInit { preInit = 1; };
         class inRange;
         class Interaction_loop;
         class Interaction_addAction;
         class draw3D;
-        class 
+        class addKeyDownEH;
+        class addMissionEvent;
+        class initKeyDownEH { preInit = 1; };
+        class removeKeyDownEH;
+        class triggerMissionEventhandler;
     };
 };
